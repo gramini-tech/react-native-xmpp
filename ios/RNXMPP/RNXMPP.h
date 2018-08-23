23069f907b26b548ea9a8ec119d960760dcf0a3a
vipinr <vipinr@houm.me>
2018-08-23 20:46:49 +0530

    Cleanup of the IOS dependencies

--- ios/RNXMPP/RNXMPP.h
+++ ios/RNXMPP/RNXMPP.h
@@ -5,9 +5,13 @@
 //  Created by Pavlo Aksonov on 23.09.15.
 //  Copyright © 2015 Pavlo Aksonov. All rights reserved.
 //
+#if __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
+#import <React/RCTBridgeModule.h>
+#endif
 
 #import <Foundation/Foundation.h>
-#import <React/RCTBridgeModule.h>
 #import <React/RCTBridge.h>
 #import <React/RCTEventDispatcher.h>
 #import <React/RCTConvert.h>
